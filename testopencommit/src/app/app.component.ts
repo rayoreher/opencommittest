eb90a16d082fc37b5e6c9b965db815571fadda73
ray_orellana <ray.orellana@agictech.com>
2023-03-30 18:18:00 +0200

    feat(app.component.ts): add footer component with async title input refactor(app.component.ts): implement OnInit interface and move title initialization to ngOnInit method

--- testopencommit/src/app/app.component.ts
+++ testopencommit/src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
     <app-header [title]="title"></app-header>
     <input [(ngModel)]="title">
+    <app-footer *ngIf="footerTitle | async as t" [title]="t"></app-footer>
   `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testopencommit';
+  footerTitle: Observable<string> | null = null;
   constructor() {
+  }
+  ngOnInit(): void {
+    this.footerTitle = of('ciao come va');
   }
 }
