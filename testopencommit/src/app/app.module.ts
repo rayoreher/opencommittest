cd9fc5a20df8633d11ab25171b85b55b7719faa3
ray_orellana <ray.orellana@agictech.com>
2023-03-30 17:50:21 +0200

    feat(app.module.ts): add FormsModule and FooterComponent to declarations feat(footer.component.html): add footer content test(footer.component.spec.ts): add test for FooterComponent creation
    feat(footer.component.ts): add footer component to the project

--- testopencommit/src/app/app.module.ts
+++ testopencommit/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
